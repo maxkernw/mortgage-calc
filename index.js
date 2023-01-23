abd29952f2d2d15c59023a60e4c97c0ac5363884
Maximilian Kern <maxkernw@gmail.com>
2023-01-23 15:02:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function () {
         navigator.serviceWorker
-            .register("/sw.js")
+            .register("/sw.js", {
+                scope: "."
+            })
             .then(res => console.log("service worker registered"))
             .catch(err => console.log("service worker not registered", err))
     })
